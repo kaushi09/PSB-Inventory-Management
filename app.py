a49cad765bcdf562383c5cc6a73a9b5d343bdc74
Kasun Gihan <kasungihan.dev@gmail.com>
2021-09-23 00:42:29 +0530

    login part

--- app.py
+++ app.py
@@ -1,7 +1,59 @@
-import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import *
+from tkinter import messagebox
 
-root = tk.Tk()
+import mysql.connector
+
+
+class Login:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("PSB-Inventory-Management")
+        self.master.geometry("1350x700+0+0")
+        self.frame = Frame(master)
+        self.frame.pack()
+
+        self.username = Label(self.frame, text="Username")
+        self.username.grid(row=0, column=0)
+
+        self.password = Label(self.frame, text="Password")
+        self.password.grid(row=1, column=0)
+
+        self.usernameText = Entry(self.frame)
+        self.usernameText.grid(row=0, column=1)
+
+        self.passwordText = Entry(self.frame)
+        self.passwordText.grid(row=1, column=1)
+
+        def addItem():
+            if self.usernameText.get() == "" or self.passwordText.get() == "":
+                messagebox.showerror(
+                    "Error", "Username and Password field are required", parent=self.frame)
+            else:
+                cnx = mysql.connector.connect(user='root', password='12345678',
+                                              host='127.0.0.1', database='ims', auth_plugin='mysql_native_password')
+                mycursor = cnx.cursor()
+                mycursor.execute(
+                    "SELECT * FROM admins WHERE username = '%s' AND password = '%s' LIMIT 1" % (self.usernameText.get(), self.passwordText.get()))
+                myresult = mycursor.fetchall()
+
+                if myresult:
+                    messagebox.showinfo(
+                        "welcome", "Login ", parent=self.frame)
+                else:
+                    messagebox.showerror(
+                        "Error", "Invalid username and password", parent=self.frame)
+
+                cnx.close()
+
+        self.loginButton = Button(self.frame, text="Login", command=addItem)
+        self.loginButton.grid(row=2, column=1)
+
+
+master = Tk()
+obj = Login(master)
+master.mainloop()
+
+""" root = tk.Tk()
 
 root.title("PSB-Inventory-Management")
 
@@ -20,8 +72,11 @@ passwordText.pack()
 
 
 def addItem():
-    res = tk.messagebox.askquestion(
-        "Add Item", "Are you sure you want to add this item?")
+    if usernameText.get() == "" or passwordText.get() == "":
+    res = tk.messagebox.showerror(
+        "Error", "Username and Password field are required")
+    res.pack()
+
     print(usernameText.get())
 
 
@@ -31,4 +86,126 @@ canvas.pack()
 submitBtn = tk.Button(root, text="Submit", command=addItem)
 submitBtn.pack()
 
-root.mainloop()
+root.mainloop() """
+
+# import modules
+"""  
+from tkinter import *
+import os
+ 
+
+ 
+ 
+# Designing window for login 
+ 
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter your username and passward to login").pack()
+    Label(login_screen, text="").pack()
+ 
+    global username_verify
+    global password_verify
+ 
+    username_verify = StringVar()
+    password_verify = StringVar()
+ 
+    global username_login_entry
+    global password_login_entry
+ 
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
+ 
+
+ 
+# Implementing event on login button 
+ 
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+ 
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+ 
+        else:
+            password_not_recognised()
+ 
+    else:
+        user_not_found()
+ 
+# Designing popup for login success
+ 
+def login_sucess():
+    global login_success_screen
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("150x100")
+    Label(login_success_screen, text="Login Success").pack()
+    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+ 
+# Designing popup for login invalid password
+ 
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+ 
+# Designing popup for user not found
+ 
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+ 
+# Deleting popups
+ 
+def delete_login_success():
+    login_success_screen.destroy()
+ 
+ 
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+ 
+ 
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+ 
+ 
+# Designing Main(first) window
+ 
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+    main_screen.geometry("300x250")
+    main_screen.title("Account Login")
+    Label(text="Login to gain Access", bg="white", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Button(text="Login", height="2", width="30", command = login).pack()
+    Label(text="").pack()
+    
+ 
+    main_screen.mainloop()
+ 
+ 
+main_account_screen() """
