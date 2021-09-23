728d5b9fc3ddff5be5731b69c7867d3b7880b9d5
Kasun Gihan <kasungihan.dev@gmail.com>
2021-09-24 02:14:35 +0530

    item delete

--- items.py
+++ items.py
@@ -9,38 +9,6 @@ class Item:
         self.master = master
         self.master.title("PSB-Inventory-Management")
         self.master.geometry("1350x700+0+0")
-        self.frame = Frame(master)
-        self.frame.pack()
-
-        self.nameLabel = Label(self.frame, text="Name")
-        self.nameLabel.grid(row=0, column=0)
-
-        self.cateLabel = Label(self.frame, text="Category")
-        self.cateLabel.grid(row=1, column=0)
-
-        self.qtyLabel = Label(self.frame, text="Qty")
-        self.qtyLabel.grid(row=2, column=0)
-
-        self.priceLabel = Label(self.frame, text="Price")
-        self.priceLabel.grid(row=3, column=0)
-
-        self.dateLabel = Label(self.frame, text="Date")
-        self.dateLabel.grid(row=4, column=0)
-
-        self.name = Entry(self.frame)
-        self.name.grid(row=0, column=1)
-
-        self.cate = Entry(self.frame)
-        self.cate.grid(row=1, column=1)
-
-        self.qty = Entry(self.frame)
-        self.qty.grid(row=2, column=1)
-
-        self.price = Entry(self.frame)
-        self.price.grid(row=3, column=1)
-
-        self.date = Entry(self.frame)
-        self.date.grid(row=4, column=1)
 
         def addItem():
             if self.name.get() == "":
@@ -75,16 +43,12 @@ class Item:
 
                     con.close()
 
-        self.loginButton = Button(self.frame, text="Add Item", command=addItem)
-        self.loginButton.grid(row=5, column=1)
-
-        self.search = Entry(self.frame)
-        self.search.grid(row=6, column=0)
-
-        self.framet = Frame(master)
-        self.framet.pack()
-
         def searchItem():
+            self.framet.destroy()
+
+            self.framets = Frame(master)
+            self.framets.pack()
+
             try:
                 con = mysql.connector.connect(
                     host="localhost",
@@ -94,22 +58,25 @@ class Item:
                     auth_plugin='mysql_native_password'
                 )
                 cur = con.cursor()
-                cur.execute("select * from items where name like %s",
-                            (self.search.get(),))
-
+                cur.execute("SELECT * FROM items WHERE name LIKE " +
+                            "'"+self.search.get()+"%'")
                 lst = cur.fetchall()
-                # find total number of rows and
-                # columns in list
-                total_rows = len(lst)
-                total_columns = len(lst[0])
+                print(lst)
+                if len(lst) == 0:
+                    messagebox.showerror("Error", "No item found")
+                else:
+                    # find total number of rows and
+                    # columns in list
+                    total_rows = len(lst)
+                    total_columns = len(lst[0])
 
-                # code for creating table
-                for i in range(total_rows):
-                    for j in range(total_columns):
-                        self.table = Entry(self.framet, width=20, fg='blue',
-                                           font=('Arial', 16, 'bold'))
-                        self.table.grid(row=i, column=j)
-                        self.table.insert(END, lst[i][j])
+                    # code for creating table
+                    for i in range(total_rows):
+                        for j in range(total_columns):
+                            self.tables = Entry(self.framets, width=20, fg='blue',
+                                                font=('Arial', 16, 'bold'))
+                            self.tables.grid(row=i, column=j)
+                            self.tables.insert(END, lst[i][j])
 
                 con.commit()
             except Exception as e:
@@ -118,11 +85,10 @@ class Item:
 
                 con.close()
 
-        self.searchButton = Button(
-            self.frame, text="Search", command=searchItem)
-        self.searchButton.grid(row=6, column=1)
-
         def getItem():
+
+            self.framet = Frame(master)
+            self.framet.pack()
 
             try:
                 con = mysql.connector.connect(
@@ -139,7 +105,6 @@ class Item:
                 # find total number of rows and
                 # columns in list
                 total_rows = len(lst)
-                print(total_rows)
                 total_columns = len(lst[0])
 
                 # code for creating table
@@ -157,6 +122,72 @@ class Item:
 
                 con.close()
 
+        def deleteItem():
+            try:
+                con = mysql.connector.connect(
+                    host="localhost",
+                    user="root",
+                    passwd="12345678",
+                    database="ims",
+                    auth_plugin='mysql_native_password'
+                )
+                cur = con.cursor()
+                cur.execute("delete from items where name = %s",
+                            (self.search.get(),))
+                con.commit()
+                messagebox.showinfo("Success", "Item deleted")
+            except Exception as e:
+                con.rollback()
+                messagebox.showerror("Error", e)
+
+                con.close()
+
+        # Form---------------------------
+        self.frame = Frame(master)
+        self.frame.pack()
+
+        self.nameLabel = Label(self.frame, text="Name")
+        self.nameLabel.grid(row=0, column=0)
+
+        self.cateLabel = Label(self.frame, text="Category")
+        self.cateLabel.grid(row=1, column=0)
+
+        self.qtyLabel = Label(self.frame, text="Qty")
+        self.qtyLabel.grid(row=2, column=0)
+
+        self.priceLabel = Label(self.frame, text="Price")
+        self.priceLabel.grid(row=3, column=0)
+
+        self.dateLabel = Label(self.frame, text="Date")
+        self.dateLabel.grid(row=4, column=0)
+
+        self.name = Entry(self.frame)
+        self.name.grid(row=0, column=1)
+
+        self.cate = Entry(self.frame)
+        self.cate.grid(row=1, column=1)
+
+        self.qty = Entry(self.frame)
+        self.qty.grid(row=2, column=1)
+
+        self.price = Entry(self.frame)
+        self.price.grid(row=3, column=1)
+
+        self.date = Entry(self.frame)
+        self.date.grid(row=4, column=1)
+
+        self.loginButton = Button(self.frame, text="Add Item", command=addItem)
+        self.loginButton.grid(row=5, column=1)
+
+        # Search---------------------------
+        self.search = Entry(self.frame)
+        self.search.grid(row=6, column=0)
+
+        self.searchButton = Button(
+            self.frame, text="Search", command=searchItem)
+        self.searchButton.grid(row=6, column=1)
+
+        # Table---------------------------
         getItem()
 
 
